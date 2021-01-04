b9a20fdd8dbc769ad479842e863c039e9260085b
Joe May <joemaybooking@gmail.com>
2021-01-04 13:47:07 -0800

    fixed spelling

--- src/components/aboutUs/aboutUs.jsx
+++ src/components/aboutUs/aboutUs.jsx
@@ -24,18 +24,12 @@ class AboutUs extends Component {
           <Navbar/>
           <img class="collage" src='https://studypupassets.s3-us-west-1.amazonaws.com/StudyPup_assets/BeFunky-collage1.png' alt="" />
           <h2 class="fam">About StudyPup</h2>
-              <p className="select">Study Pup is an edu-tainment company designed for children 1st-4rd grade to learn common core standard and have a blast doing so. 
-              We provide Videos, Aracde games, and most of all, our Story Game "StudyPup and the Quest for the Mystic Badges!" This game takes children on a journey through the galaxy as our protagantist Nova earns pins to save the galaxy. 
-              Each level increases difficultly allowing the child to progress in the subject all the while progessing in the story.  </p>
+              <p className="select">Study Pup is an edutainment company designed to help 1st - 4rd graders learn common core standards, and have a blast doing so! We provide educational videos and arcade games, as well as our flagship Story Game "StudyPup and the Quest for the Mystic Badges!" This game takes children on a journey through the galaxy, playing math games to help our protagonist Nova save the universe. Each level increases difficultly, allowing the child to progress in math skills while progressing in the story.  </p>
 
           
               <img class="familyPic" src='https://studypupassets.s3-us-west-1.amazonaws.com/StudyPup_assets/IMG_1348.jpeg' alt="" />
               <h2 class="fam">The Story</h2>
-              <p className="select">Karen May founded StudyPup when she was a 1st grade teacher in the San Fransico Bay Area. 
-              She found her self trying to find extra resorces for her students beyond what her class room could offer. 
-              After weeks of struggling to find content that was entertaining, ingaging, funny, and followed the common core standards she came up with an idea to create StudyPup.
-              Her Sons, Joe and Josh having the same passion for the vision, both went to school to become programmers to make studyPup a reality. With the prospective of an educator, 
-              the technial know-how of her sons, and the story telling ability of the talented Aubrey Huntsman, and the adorble face of their dog Obie StudyPup was born.
+              <p className="select">Karen May founded StudyPup when she was teaching 1st grade in the San Francisco Bay Area. One day she found herself looking to find engaging resources for her students beyond what her classroom could offer. After weeks of struggling to find content that was entertaining, funny, and taught the common core standards, she envisioned StudyPup. Her sons, Joe and Josh, having the same passion for the vision, went to computer programming school to make studyPup a reality. With the perspective of an educator, the technical know-how of Joe and Josh, and the story-telling ability of the talented Aubrey Huntsman, (not to forget the adorable face of their real-life dog Obie!) StudyPup was born!
 
              </p>
  
